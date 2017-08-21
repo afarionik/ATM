44d34645071840ba2d4395dc8c2db507b93cbfa3
afarionik <vaeritas6055@gmail.com>
2017-08-21 20:07:36 +0300

    test_money_1

--- test_money.py
+++ test_money.py
@@ -7,5 +7,11 @@ class TestMoney(unittest.TestCase):
         sal = Atm(ask_money=1000, exist_money=9000)
         self.assertEqual(1000, sal.get_money())
         
+    def test_neg_flow(self):
+        we_ask_money =10000
+        sal = Atm(ask_money=we_ask_money, exist_money=9000)
+        self.assertIsNone(sal.get_money())     
+     
+    
 if __name__ == '__main__':
     unittest.main()
